5e80b930d654160f70aa5f4bef901fb62acb1c6a
Matthew Hall <matt.hall@cashflows.com>
2025-01-24 10:23:44 +0000

    Shellcheck resolutions

--- shini.sh
+++ shini.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 # shini - compatible INI library for sh
 #
 # This code is released freely under the MIT license - see the shipped LICENSE document.
@@ -32,9 +33,11 @@ shini_function_exists()
 shini_regex_match()
 {
     # $KSH_VERSION (I'm told) only exists on ksh 93 and above, which supports regex matching.
-    if [ -n "$BASH_VERSINFO" ] && [ "$BASH_VERSINFO" -ge 3 ] || \
+    # shellcheck disable=SC3054
+    if [ -n "${BASH_VERSINFO[0]}" ] && [ "${BASH_VERSINFO[0]}" -ge 3 ] || \
        [ -n "$ZSH_VERSION" ] || \
        [ -n "$KSH_VERSION" ]; then
+        # shellcheck disable=SC3010
         [[ "$1" =~ $2 ]] && return 0 || return 1
     fi
 
@@ -44,8 +47,12 @@ shini_regex_match()
 
 shini_regex_replace()
 {
-    if [ -n "$BASH_VERSINFO" ] && [ "${BASH_VERSINFO}" -ge 3 ] || \
+    # shellcheck disable=SC3054
+    if [ -n "${BASH_VERSINFO[0]}" ] && [ "${BASH_VERSINFO[0]}" -ge 3 ] || \
        [ -n "$ZSH_VERSION" ]; then
+        # shellcheck disable=SC3010
+        # shellcheck disable=SC3028
+        # shellcheck disable=SC3054
         [[ "$1" =~ $2 ]] && shini_retval=${BASH_REMATCH[1]} || shini_retval="$1"
         return 0
     fi
@@ -85,23 +92,23 @@ shini_parse_section()
     EXTRA3=''
     SECTION_FOUND=-1
 	
-	if [ $# -ge 2 ] && [ ! -z "$2" ]; then
+	if [ $# -ge 2 ] && [ -n "$2" ]; then
         SKIP_TO_SECTION="$2"
     fi
 	
-    if [ $# -ge 3 ] && [ ! -z "$3" ]; then
+    if [ $# -ge 3 ] && [ -n "$3" ]; then
         POSTFIX="_$3"
     fi
 	
-    if [ $# -ge 4 ] && ! [ -z "$4" ]; then
+    if [ $# -ge 4 ] && [ -n "$4" ]; then
         EXTRA1="$4"
     fi
 	
-    if [ $# -ge 5 ] && [ ! -z "$5" ]; then
+    if [ $# -ge 5 ] && [ -n "$5" ]; then
         EXTRA2="$5"
     fi
 	
-    if [ $# -ge 6 ] && [ ! -z "$6" ]; then
+    if [ $# -ge 6 ] && [ -n "$6" ]; then
         EXTRA3="$6"
     fi
 	
@@ -124,6 +131,7 @@ shini_parse_section()
         if shini_function_exists "__shini_file_unreadable${POSTFIX}"; then
             "__shini_file_unreadable${POSTFIX}" "$INI_FILE" "$EXTRA1" "$EXTRA2" "$EXTRA3"
         else
+            # shellcheck disable=SC2016
             printf 'shini: Unable to read INI file:\n  `%s`\n' "$INI_FILE" 1>&2
             exit 253
         fi
@@ -132,7 +140,7 @@ shini_parse_section()
     # Iterate INI file line by line
     LINE_NUM=0
     SECTION=''
-    while read LINE || [ -n "$LINE" ]; do  # -n $LINE catches final line if not empty
+    while read -r LINE || [ -n "$LINE" ]; do  # -n $LINE catches final line if not empty
         # Check for new sections
         if shini_regex_match "$LINE" "^${RX_WS}*\[${RX_SECTION}${RX_SECTION}*\]${RX_WS}*$"; then
             shini_regex_replace "$LINE" "^${RX_WS}*\[(${RX_SECTION}${RX_SECTION}*)\]${RX_WS}*$" "\1"
@@ -140,7 +148,7 @@ shini_parse_section()
 
             if [ "$SKIP_TO_SECTION" != '' ]; then
                 # stop once specific section is finished
-                [ $SECTION_FOUND -eq 0 ] && break;
+                [ "$SECTION_FOUND" -eq 0 ] && break;
                 
                 # mark the specified section as found
                 [ "$SKIP_TO_SECTION" = "$SECTION" ] && SECTION_FOUND=0;
@@ -155,7 +163,7 @@ shini_parse_section()
         fi
         
         # Skip over sections we don't care about, if a specific section was specified
-        [ "$SKIP_TO_SECTION" != '' ] && [ $SECTION_FOUND -ne 0 ] && LINE_NUM=$((LINE_NUM+1)) && continue;
+        [ "$SKIP_TO_SECTION" != '' ] && [ "$SECTION_FOUND" -ne 0 ] && LINE_NUM=$((LINE_NUM+1)) && continue;
 		
         # Check for new values
         if shini_regex_match "$LINE" "^${RX_WS}*${RX_KEY}${RX_KEY}*${RX_WS}*="; then
@@ -191,6 +199,7 @@ shini_parse_section()
             if shini_function_exists "__shini_parse_error${POSTFIX}"; then
                 "__shini_parse_error${POSTFIX}" $LINE_NUM "$LINE" "$EXTRA1" "$EXTRA2" "$EXTRA3"
             else
+                # shellcheck disable=SC2016
                 printf 'shini: Unable to parse line %d:\n  `%s`\n' $LINE_NUM "$LINE" 1>&2
             fi
         fi
@@ -206,6 +215,7 @@ shini_parse_section()
 # @param section Section of INI file to write to
 # @param variable Variable name to add/update/delete
 # @param value Value to add/update, do not specify to delete
+# @param quote (Double-)quote the value being written (default: false)
 shini_write()
 {
     shini_setup
@@ -222,12 +232,13 @@ shini_write()
     #  __shini_parse_error__writer()
     
     # Writer callbacks, used for writing the INI file content
+    # shellcheck disable=SC2317
     __shini_parsed_section__writer()
     {
         # Validate the last section wasn't the target section
         if [ "$LAST_SECTION" = "$WRITE_SECTION" ]; then
             # If it was, and the value wasn't written already, write it
-            if [ $VALUE_WRITTEN -eq 0 ]; then
+            if [ "$VALUE_WRITTEN" -eq 0 ]; then
                 printf "\n%s=%s" "$WRITE_KEY" "$WRITE_VALUE" >> "$INI_FILE_TEMP"
                 VALUE_WRITTEN=1
             fi
@@ -237,24 +248,28 @@ shini_write()
         LAST_SECTION="$1"
     }
     
+    # shellcheck disable=SC2317
     __shini_parsed_comment__writer()
     {
         printf ";%s" "$1" >> "$INI_FILE_TEMP"
     }
     
+    # shellcheck disable=SC2317
     __shini_parsed__writer()
     {
         if [ "$1" = "$WRITE_SECTION" ]; then
             if [ "$2" = "$WRITE_KEY" ]; then
-                if [ ! -z "$WRITE_VALUE" ]; then
-                    printf "\n%s=%s" "$WRITE_KEY" "$WRITE_VALUE" >> "$INI_FILE_TEMP"
+                if [ -n "$WRITE_VALUE" ]; then
+                    # shellcheck disable=SC2059
+                    printf "$PRINTFMT y" "$WRITE_KEY" "$WRITE_VALUE" >> "$INI_FILE_TEMP"
                 fi
                 VALUE_WRITTEN=1
                 return
             fi
         fi
-        
-        printf "\n%s=%s" "$2" "$3" >> "$INI_FILE_TEMP"
+
+        # shellcheck disable=SC2059
+        printf "$PRINTFMT" "$2" "$3" >> "$INI_FILE_TEMP"
     }
     
     if [ $# -lt 3 ]; then
@@ -274,16 +289,24 @@ shini_write()
     WRITE_VALUE="$4"
     LAST_SECTION=""
     VALUE_WRITTEN=0
+
+    # "Quote value" mode (true/false)
+    if [ -n "$5" ] && [ "$5" = true ]; then
+        PRINTFMT='\n%s="%s"'
+    else
+        PRINTFMT='\n%s=%s'
+    fi
     
     shini_parse "$1" "_writer" "$2" "$3" "$4"
     # Still not written out yet
-    if [ $VALUE_WRITTEN -eq 0 ]; then
+    if [ "$VALUE_WRITTEN" -eq 0 ]; then
         # Check if final existing section was target one, add it if not
         if [ "$LAST_SECTION" != "$WRITE_SECTION" ]; then
             printf "\n[%s]" "$WRITE_SECTION" >> "$INI_FILE_TEMP"
         fi
         # Write value at end of file
-        printf "\n%s=%s" "$WRITE_KEY" "$WRITE_VALUE" >> "$INI_FILE_TEMP"
+        # shellcheck disable=SC2059
+        printf "$PRINTFMT" "$WRITE_KEY" "$WRITE_VALUE" >> "$INI_FILE_TEMP"
     fi
     
     mv "$INI_FILE_TEMP" "$INI_FILE"
